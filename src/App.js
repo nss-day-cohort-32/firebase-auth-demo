844b429f08ff25732554e6ae81bec8176088ff97
Adam <adam.e.sheaffer@gmail.com>
2019-07-28 19:56:32 -0500

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Home from './components/Home';
+import { getUserFromLocalStorage, logout } from './auth/userManager';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    user: getUserFromLocalStorage()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Route path="/login" render={(props) => <Login {...props} onLogin={(user) => this.setState({ user: user })} />} />
+          <Route path="/register" render={(props) => <Register {...props} onRegister={(user) => this.setState({ user: user })} />} />
+          <Route exact path="/" render={(props) => {
+            return this.state.user ? (
+              <Home {...props} user={this.state.user} onLogout={logout} />
+            ) : (
+                <Redirect to="/login" />
+              )
+          }} />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
