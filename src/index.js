e977e554836e61d833e2d682a118e5649e5c0f00
Adam <adam.e.sheaffer@gmail.com>
2019-07-28 20:26:02 -0500

    add firebase sdk

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as firebase from 'firebase/app';
+import { firebaseConfig } from './config/firebase';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
